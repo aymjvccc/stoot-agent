06a509e1ec7e068f692ab40f15ebde74ca36af3a
aymjvccc <aymenss2005@gmail.com>
2025-04-09 16:46:06 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>STOOT AGENT ONLINE!</h1>
+      <p>This site is live on Vercel!</p>
+    </div>
+  );
+}
+
+export default App;
